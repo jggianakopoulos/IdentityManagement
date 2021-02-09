b55ef9ffb140fd13515be399457be0deaeb499de
Jacob <jggianakopoulos@gmail.com>
2021-02-08 19:50:06 -0500

    Server code

--- ServerCode/login.php
+++ ServerCode/login.php
@@ -1,27 +1,41 @@
 <?php
-
 	
    $email = $_POST['email'];
    $password = $_POST['password'];
+   $error_message = "";
 
    if (is_null($email) || is_null($password)) {
-      echo 0;
+      $error_message = "You did not submit all of the required information";
    } else {
       try {
-      
+      	// We should reconsider the security of all of this. Especially the echoing of user info
+      	// Also add an api access user to the database and change the line below
          $pdo = new PDO("mysql:host=localhost;dbname=identitymanagement", 'root', "");
          $stmt = $pdo->prepare("select * from user where email = ? and password = ?");
          $stmt->execute(array($email, $password));
          $user = $stmt->fetch(PDO::FETCH_ASSOC);
          if (!is_null($user) && array_key_exists("user_id", $user)) {
-         	echo $user["user_id"];
+         	echo json_encode(array(
+         		"user" => $user,
+         		"user_id" => $user["user_id"],
+         		"error_message" => ""
+         	));
          } else {
-         	echo 0;
+         	$error_message = "A user does not exist with these credentials";
          }
 
       } catch (Exception $e) {
-         echo 0;
+         $error_message = "An error occurred with your login";
       }
+   }
+
+
+   if ($error_message != "") {
+   		echo json_encode(array(
+   			"error_message" => $error_message,
+   			"user_id" => 0,
+   			"user" => ""
+   		));
    }
 
 
--- ServerCode/profileupdate.php
+++ ServerCode/profileupdate.php
@@ -1,30 +1,31 @@
 <?php
 
-   $email = $_POST['email'];
+   $email = $_POST['old_email'];
+   $new_email = $_POST['email'];
    $first_name = (array_key_exists('first_name', $_POST)) ? $_POST["first_name"] : "";
    $last_name = (array_key_exists('last_name', $_POST)) ? $_POST["last_name"] : "";
    $password = $_POST['password'];
-   $confirm_password = $_POST['confirm_password'];
    $phone_number = (array_key_exists('phone_number', $_POST)) ? $_POST["phone_number"] : "";
 
    $error_message = "";
 
-   if (is_null($email) || is_null($password) || is_null($confirm_password) || $password != $confirm_password) {
+   if (is_null($new_email) || is_null($password)) {
       $error_message = "You must include a valid email and password";
    } else {
       try {
       	$pdo = new PDO("mysql:host=localhost;dbname=identitymanagement", 'root', "");
-         $stmt = $pdo->prepare("select * from user where email = ?");
-         $stmt->execute(array($email));
+         $stmt = $pdo->prepare("select * from user where email = ? and password = ?");
+         $stmt->execute(array($email, $password));
          $user = $stmt->fetch(PDO::FETCH_ASSOC);
-         if ($user) {
-         	$error_message = "A user with this email already exists";
+
+         if (!$user || is_null($user)) {
+         	$error_message = "Incorrect Password";
          } else {
-	         $stmt = $pdo->prepare("insert into user (email, password, first_name, last_name, phone_number) values (?,?,?,?,?)");
-	         $stmt->execute(array($email, $password, $first_name, $last_name, $phone_number));
+	         $stmt = $pdo->prepare("update user set email = ?, password = ?, first_name = ?, last_name = ?, phone_number = ? where user_id = ?");
+	         $stmt->execute(array($new_email, $password, $first_name, $last_name, $phone_number, $user["user_id"]));
 
             $stmt = $pdo->prepare("select * from user where email = ?");
-            $stmt->execute(array($email));
+            $stmt->execute(array($new_email));
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
 	         
             echo json_encode(array(
