135899e4ac7abba34c4b78502f7b789e3f194467
Jacob <jggianakopoulos@gmail.com>
2021-03-31 14:59:35 -0400

    settings

--- API/config/settings.php
+++ API/config/settings.php
@@ -1,6 +1,6 @@
 <?php
 
 $server = "34.69.148.52";
-$userdatapath = "/var/www/html/idm/assets/userdata/";
+$userdatapath = "/var/www/idm/API/assets/userdata/";
 $userfacepath = $userdatapath . "faces/";
 $facerecognitionpath = "/home/steverobertscott/.virtualenvs/dlib/bin/face_recognition";
