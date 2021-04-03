d085e60395f103fae16406039645e8feb648ff14
Jacob <jggianakopoulos@gmail.com>
2021-04-02 22:17:22 -0400

    camera

---
+++ AndroidApp/app/src/main/java/com/group/idm2/Activities/CameraActivity.java
@@ -0,0 +1,131 @@
+package com.group.idm2.Activities;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.group.idm2.R;
+import com.group.idm2.Tasks.FaceTask;
+
+public class CameraActivity extends AbstractActivity implements SurfaceHolder.Callback {
+    TextView testView;
+
+    Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+
+    PictureCallback rawCallback;
+    ShutterCallback shutterCallback;
+    PictureCallback jpegCallback;
+    SharedPreferences sharedPreferences;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_camera);
+        sharedPreferences = this.getSharedPreferences("preferences",MODE_PRIVATE);
+
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        surfaceHolder = surfaceView.getHolder();
+
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        jpegCallback = new PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera camera) {
+                System.out.println(data);
+                try {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                    new FaceTask(getApplicationContext(), sharedPreferences.getString("email", ""), sharedPreferences.getString("password", ""),bitmap).execute();
+                } catch (Exception e) {
+                    showToast(getApplicationContext(), "An error occurred when taking your picture. Please try again");
+                    e.printStackTrace();
+                }
+                surfaceDestroyed(surfaceHolder);
+            }
+        };
+    }
+
+    public void captureImage(View v) throws IOException {
+
+        camera.takePicture(null, null, jpegCallback);
+    }
+
+    public void refreshCamera() {
+        if (surfaceHolder.getSurface() == null) {
+
+            return;
+        }
+
+
+        try {
+            camera.stopPreview();
+        } catch (Exception e) {
+
+        }
+
+
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+
+        refreshCamera();
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+
+            camera = Camera.open();
+        } catch (RuntimeException e) {
+
+            System.err.println(e);
+            return;
+        }
+        Camera.Parameters param;
+        param = camera.getParameters();
+
+
+        param.setPreviewSize(352, 288);
+        camera.setParameters(param);
+        try {
+
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (Exception e) {
+
+            System.err.println(e);
+            return;
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // stop preview and release camera
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    }
+}
--- AndroidApp/app/src/main/java/com/group/idm2/Activities/FaceActivity.java
+++ AndroidApp/app/src/main/java/com/group/idm2/Activities/FaceActivity.java
@@ -1,8 +1,11 @@
 package com.group.idm2.Activities;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -10,6 +13,9 @@ import android.widget.Button;
 
 import com.group.idm2.Tasks.FaceTask;
 import com.group.idm2.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 
 public class FaceActivity extends AbstractActivity {
     Button faceNormal, faceLeft, faceRight;
@@ -28,9 +34,27 @@ public class FaceActivity extends AbstractActivity {
 
     }
 
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public void openCamera(View view) {
-        Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(camera_intent, 10);
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        }
+        Intent send = new Intent(this, CameraActivity.class);
+        this.startActivity(send);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showToast(this, "Camera Permission has been granted");
+            } else {
+               showToast(this, "Camera permission has been denied. You cannot access this feature without approving it.");
+            }
+        }
     }
 
     @Override
