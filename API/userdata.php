f62df01c082e49bbe71830e0eb26d4472626ac1e
Jacob <jggianakopoulos@gmail.com>
2021-04-18 17:04:35 -0400

    progress

--- API/userdata.php
+++ API/userdata.php
@@ -1,9 +1,9 @@
 <?php
-
+require("config/settings.php");
 $client_secret = "eacf1535c5ca11e8d59189c08688596a";
 $token = $_POST["authtoken"];
 
-$ch = curl_init ( 'localhost/identitymanagement/api/developer/convert_token.php' );
+$ch = curl_init ( 'http://$server/api/developer/convert_token.php' );
 curl_setopt_array ( $ch, array (
     CURLOPT_POST => 1,
     CURLOPT_POSTFIELDS => array (
