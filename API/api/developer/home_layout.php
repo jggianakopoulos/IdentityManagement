5c79caa2aad4d7152e60727ba89578f1583f1388
Jacob <jggianakopoulos@gmail.com>
2021-04-19 00:28:58 -0400

    fix some styling

--- API/api/developer/home_layout.php
+++ API/api/developer/home_layout.php
@@ -1,6 +1,6 @@
 <?php require("../../config/settings.php")?>
 <!DOCTYPE html>
-<html class="background-color">
+<html class="background-color" style="background-color: #fbfbfb">
 <head>
     <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script src="../sha256.js" type="text/javascript"></script>
@@ -8,36 +8,46 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 
 </head>
-<body class="background-color">
-<div>
-    <div class="container" style="display: block;padding: 0 100px;">
-            <div class='content'>
-                <div style="flex-direction: row;display: flex;">
-                    <div class="tab">IdentityManagement</div>
+<body class="background-color" style="
+    background-color: #fbfbfb;
+">
+<div style="background-color: #fbfbfb"><div style="margin: auto;background-color: #c3c3c3;padding-top: 80px;max-width: 1140px;font-weight: bold;display: flex;font-size: 30px;padding-left: 20px;"><img style="width: 50px;padding: 5px;" src="../user.svg"><div style="align-self: flex-end;padding-left: 10px;align-items: end;">IdentityManagement</div>
+    </div></div>
+<div class="container" style="display: block;padding: 0;margin: auto;background-color: #fbfbfb;">
+    <div style="
+    background-color: #e8e8e8;
+    display: flex;
+    flex-direction: column;
+
+    ">
+        <div style="flex-direction: row;display: flex;">
+
+        </div>
+        <div id="login">
+            <div>
+                <div class="display-field">
+                    <label class="display-label" for='email'>Email</label>
+                    <div name="email" id='email'><?php echo $_SESSION["email"]; ?></div>
                 </div>
-                <div id="login">
-                    <div>
-                        <div class="display-field">
-                            <label class="display-label" for='email'>Email</label>
-                            <div name="email" id='email'><?php echo $_SESSION["email"]; ?></div>
-                        </div>
-                        <div class="display-field">
-                            <label class="display-label" for='Company'>Company</label>
-                            <div name="company" id='company'><?php echo $_SESSION["company"]; ?></div>
-                        </div>
-                        <div class="display-field">
-                            <label class="display-label" for='client_id'>Client ID</label>
-                            <div id='client_id'><?php echo $_SESSION["client_id"]; ?></div>
-                        </div>
-                        <div class="display-field">
-                            <label class="display-label" for='client_secret'>Client Secret</label>
-                            <div id='client_secret'><?php echo $_SESSION["client_secret"]; ?></div>
-                        </div>
-                    </div>
+                <div class="display-field">
+                    <label class="display-label" for='Company'>Company</label>
+                    <div name="company" id='company'><?php echo $_SESSION["company"]; ?></div>
+                </div>
+                <div class="display-field">
+                    <label class="display-label" for='client_id'>Client ID</label>
+                    <div id='client_id'><?php echo $_SESSION["client_id"]; ?></div>
+                </div>
+                <div class="display-field">
+                    <label class="display-label" for='client_secret'>Client Secret</label>
+                    <div id='client_secret'><?php echo $_SESSION["client_secret"]; ?></div>
                 </div>
             </div>
-        <a style="padding: 2px 10px;display: inline-block;background-color: #d3d4ea;border: 1px #7d7d7d solid;border-radius: 6px;margin-top: 2px;"<a href="http://<?php echo $server; ?>/api/developer/logout.php">Logout.</a>
+        </div>
     </div>
+    <a style="padding: 2px 10px;background-color: #c3c3c3;margin-top: 2px;float: right;color: black;" <a="" href="http://localhost/identitymanagement/api/developer/logout.php">Click here to logout</a>
 </div>
+
+
+
 </body>
 </html>
--- API/api/developer/login.php
+++ API/api/developer/login.php
@@ -11,37 +11,40 @@ require("../../config/settings.php");
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">       
 
 	</head>
-	<body class="background-color">
-		<div>
-			<div class="container">
-				<div class="branding" style="margin: 50px auto 0 auto;">
-					<img style="width:100px" src="../user.svg">
-				</div>
-				<div style="font-weight: 600;">Developer Login</div>
-				<form action='login_action.php'>
-					<div class='content'>
-						<div style="flex-direction: row;display: flex;">
-							<div class="tab">IdentityManagement</div>
-						</div>
-							<div id="login">
-								<div>
-									<div class="input-field">
-										<label for='email'>Email</label>
-										<input type='text' name='email' id='email'>
-									</div>
-									<div class="input-field">
-										<label for='password'>Password</label>
-										<input type='password' name='password' id='password'>
-									</div>
-								</div>
-								<div style="margin:20px 5px 5px 5px;display:flex;justify-content:center;">
-                                    <button class="btn" style="width: 100%;" id="login-btn">Login</button>
-								</div>
-							</div>
-					</div>
-				</form>
-				<div class="content" style="padding:2px 10px;display:inline-block;">No account? <a href="http://<?php echo $server; ?>/api/developer/register.php">Create one.</a></div>
-			</div>
-		</div>
-	</body>
+    <body class="background-color">
+    <div>
+        <div class="container">
+
+
+            <form action="login_action.php">
+                <div class="content">
+                    <div style="flex-direction: row;display: flex;align-items: center;align-self: center;padding-top: 20px;">
+                        <img style="width:100px" src="../user.svg"></div><div style="
+    text-align: center;
+    font-weight: bold;
+"> Developer Sign-In
+                    </div>
+
+                    <div id="login">
+                        <div>
+                            <div class="input-field">
+                                <label for="email">Email</label>
+                                <input type="text" name="email" id="email">
+                            </div>
+                            <div class="input-field">
+                                <label for="password">Password</label>
+                                <input type="password" name="password" id="password">
+                            </div>
+                        </div>
+                        <div style="margin:20px 5px 5px 5px;display:flex;justify-content:center;">
+                            <button class="btn" style="width: 100%;" id="login-btn">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            <div class="content" style="padding:2px 10px;display:inline-block;">No account? <a href="http://<?php echo $server?>/api/developer/register.php">Create one.</a></div>
+        </div>
+    </div>
+
+    </body>
 </html>
